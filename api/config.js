438beff6adab23f05244f9b18298b3794414535f
asfalfa <glkin9@gmail.com>
2022-07-18 14:02:57 +0200

    feat(NG): :sparkles: Proxying into ExpressJS

---
+++ api/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 8080, // Pport to listen on
+    domain: "http://localhost", // Custom domain
+    mongodb_srv: "mongodb+srv://asfalfa:Td6Tzx85BMQmbD9@robobot.encvz.mongodb.net/Dungeoneer?retryWrites=true&w=majority", // MongoDB connection URL
+  };
