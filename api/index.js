36fcea617208fc608de660c394c451ad1373b79d
asfalfa <glkin9@gmail.com>
2022-07-22 11:49:29 +0200

    update config path

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-const config = require('./config');
+const config = require('../config');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
