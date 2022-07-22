315b76ecc5288aa78d0a81c69f8b500dfc97422c
asfalfa <glkin9@gmail.com>
2022-07-22 11:48:22 +0200

    dfgdfg

--- api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: 8080, // Pport to listen on
-    domain: "http://localhost", // Custom domain
-    mongodb_srv: "mongodb+srv://asfalfa:Td6Tzx85BMQmbD9@robobot.encvz.mongodb.net/Dungeoneer?retryWrites=true&w=majority", // MongoDB connection URL
-  };
